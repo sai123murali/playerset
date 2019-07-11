4d7d612f99ed6f1004ffa0c3a0e6750ad0563cb3
Epuru Sai Muralidhar <32213467+sai123murali@users.noreply.github.com>
2019-07-11 15:21:29 +0530

    Update revsort.py

--- revsort.py
+++ revsort.py
@@ -1,4 +1,8 @@
-a1,b1=map(int,input("").split())
-li=list(map(str,input("").split()))
-li.sort(reverse=True)
-print(li[b1-1])
+n1,m1=map(int,input().split())
+k=input().split()
+l2=[]
+l3=[]
+for i in k:
+    l2.append(int(i))
+l3=sorted(l2)
+print(l3[-m1])
